2a73c638456bba7e15761e74ee9fe69b69e0519c
unknown <ayushking9555@gmail.com>
2024-04-02 10:40:29 +0530

    add python file

---
+++ projectt.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from plyer import notification
+import time
+
+#remainder is a File name
+#Enter Your remainder.xlsx file location 
+data = pd.read_excel (r'remainder.xlsx')
+
+
+
+df = pd.DataFrame(data,columns=['Time','Task'])    
+istime = False
+while True:
+    #This is For The time
+    t = time.localtime()
+    current_time = time.strftime("%I:%M:%S %p", t).lstrip('0')
+
+    if  current_time in df.values and istime:
+                index = df.index
+                condition = df["Time"] == current_time
+                li = index[condition]
+                lst = li.tolist()
+                #This is For the Notification
+                notification.notify(title = str(df.values[lst,1]).replace("['", "").replace("']", "")
+                                    .capitalize()+" Time",message = 'Your Task',timeout = 15)
+                istime = False
+                time.sleep(15)
+    else:
+        istime = True
+        time.sleep(1)
